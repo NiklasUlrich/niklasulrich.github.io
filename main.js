4e5c2f1ad021f000537868139f1f2295a68b880e
Snifflaz <eisfreiesspeiseeis@gmail.com>
2023-05-25 17:56:07 +0200

    Update Main.js

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 const image = document.getElementById('startimage');
 var stocks = new Stocks('IC5PFAWJJQF1CR5L');
 
-let audioctx = null
+let audioctx = null;
+let wave = null;
 
 function playNote(freq){
-    if(audioctx==null){
-        audioctx = new(AudioContext || webkitAudioContext || window.webkitAudioContext)();
-    }
-    const dur = 0.2;
+    
+    const dur = .4;
+
     const osc = audioctx.createOscillator();
+    osc.setPeriodicWave(wave);
+
     osc.frequency.value = freq;
     osc.start();
     osc.stop(audioctx.currentTime+dur);
 
     const node =audioctx.createGain();
-    node.gain.value = 0.4;
-    node.gain.linearRampToValueAtTime(0, audioctx.currentTime+dur);
+    //node.gain.value = 0;
+    //node.gain.linearRampToValueAtTime(.4, audioctx.currentTime+dur / 10);
+    node.volume = 0.4;
+
+    node.gain.linearRampToValueAtTime(0.00001, audioctx.currentTime+dur);
     osc.connect(node);
     node.connect(audioctx.destination);
 }
 
+
 async function request () {
     var result = await stocks.timeSeries({
-      symbol: 'TSLA',
+      symbol: 'TESS',
       interval: '1min',
       amount: 1000
      });
   
-     console.log(result);
      return(result);
 }
 
@@ -40,19 +45,63 @@ function delay(milliseconds){
     });
 }
 
-async function playEntries(stockData, highest, lowest){
+function mapToCurve(stockData){
+    var returnCurves = [];
+    var highestHigh = stockData[0].high;
+    var lowestHigh = highestHigh;
+    var highestLow = stockData[0].low;
+    var lowestLow = highestLow;
 
-    var range = highest - lowest
+    var lowCurve = [];
+    var highCurve = [];
+    for(var i = 1; i < stockData.length; i++){
+        var low = stockData[i].low;
+        if(highestLow < low) highestLow = low;
+        if(lowestLow > low) lowestLow = low;
+        lowCurve.push(low);
+
+        var high = stockData[i].high;
+        if(highestHigh < high) highestHigh = high;
+        if(lowestHigh > high) lowestHigh = high;
+        highCurve.push(high);
+    }
+
+    var returnLowCurve = [];
+    var returnHighCurve = [];
+    for(var i = 0; i < lowCurve.length; i++){
+        var returnLow = mapToRange(lowCurve[i], lowestLow, highestLow, -1.0, 1.0);
+        returnLowCurve.push(returnLow);
+
+        var returnHigh = mapToRange(highCurve[lowCurve.length - 1 - i], lowestHigh, highestHigh, -1.0, 1.0);
+        returnHighCurve.push(returnHigh);
+    }
+
+    returnCurves.push(returnLowCurve, returnHighCurve);
+    return returnCurves;
+}
+
+function createOscillatorWave(stockData){
+    var curves = mapToCurve(stockData);
+    console.log(curves);
+    const newWave = audioctx.createPeriodicWave(curves[0], curves[1]);
+    return newWave;
+}
+
+function mapToRange(value, minIn, maxIn, minRange, maxRange){
+    var range = maxIn - minIn;
+    var data = value;
+    var dial = (data - minIn) / range;
+    var ret = (dial * (maxRange - minRange) ) + minRange;
+
+    return ret;
+}
+
+async function playEntries(stockData, highest, lowest){
+    
+    wave = createOscillatorWave(stockData, audioctx);
 
     for(var i = 0; i < stockData.length; i++){
-        var data = stockData[i].open;
-        var dial = (data - lowest) / range;
-
-        console.log(data + ", dial to " + dial);
-
-        var freq = (dial * (maxFreq - minFreq) ) + minFreq;
-
-        console.log("playing " + freq);
+        var freq = mapToRange(stockData[i].open, lowest, highest, minFreq, maxFreq);
         
         playNote(freq);
 
@@ -64,7 +113,7 @@ async function playStock(){
     var stockData = await request();
     var lowest = stockData[0].open;
     var highest = lowest;
-    for(var i = 0; i < stockData.length; i++ ){
+    for(var i = 1; i < stockData.length; i++ ){
         var data= stockData[i].open;
         if(data < lowest) lowest = data;
         if(data > highest) highest = data;
@@ -76,5 +125,37 @@ async function playStock(){
 }
 
 image.onclick = function(){
-    playStock();
+    if(audioctx==null){
+        audioctx = new(AudioContext || webkitAudioContext || window.webkitAudioContext)();
+    }
+
+    //playStock();
+    bootKeyBoard(); 
+}
+
+async function bootKeyBoard(){
+    console.log("booting keyboard")
+    var stockData = await request();
+    wave = createOscillatorWave(stockData, audioctx);
+
+    document.addEventListener('keypress', (event) => {
+        var freq;
+        switch (event.key){
+            case 'a': freq = 130.8; break;
+            case 's': freq = 146.8; break;
+            case 'd': freq = 164.8; break;
+            case 'f': freq = 174.6; break;
+            case 'g': freq = 196.0; break;
+            case 'h': freq = 220.0; break;
+            case 'j': freq = 246.9; break;
+            default: return;
+        }
+        playNote(freq);
+      }, false);
+   // document.addEventListener("keyup", onKeyUp(wave));
+}
+
+
+function onKeyUp(wave){
+
 }
